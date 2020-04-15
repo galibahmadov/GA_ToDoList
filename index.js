c1d94a7c2ab0b4703ba870491cf7627f1b14b15e
Galib Ahmadov <galib.akhmedov@gmail.com>
2020-04-15 07:31:25 +0400

    first

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const addButton = document.querySelector('#button');
-addButton.addEventListener('click', newRow);
+addButton.addEventListener('click', addingTask);
 
 const sort = `<svg id = "top-sort"  width="25" height="15" viewBox="0 0 25 15" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="5" y="15" width="2.5" height="12.5" transform="rotate(-180 5 15)" fill="#C4C4C4" />
@@ -9,7 +9,7 @@ const sort = `<svg id = "top-sort"  width="25" height="15" viewBox="0 0 25 15" f
     <path d="M3.75 6.55671e-07L6.99759 4.6875L0.502404 4.6875L3.75 6.55671e-07Z" fill="#C4C4C4" />
 </svg>`
 
-const reverseSort =  `<svg id="reverse-sort" width="25" height="15" viewBox="0 0 25 15" fill="none"
+const oposideSide =  `<svg id="reverse-sort" width="25" height="15" viewBox="0 0 25 15" fill="none"
         xmlns="http://www.w3.org/2000/svg">
         <rect x="2.5" width="2.5" height="12.5" fill="#C4C4C4"/>
         <rect x="10" y="3.75" width="2.5" height="7.5" transform="rotate(-90 10 3.75)" fill="#C4C4C4" />
@@ -21,42 +21,42 @@ const reverseSort =  `<svg id="reverse-sort" width="25" height="15" viewBox="0 0
 const moverSvg = '<svg class="move-area" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><circle draggable="true" cx="2" cy="2" r="2" fill="#C4C4C4" /><circle draggable="true" cx="2" cy="10" r="2" fill="#C4C4C4" /><circle draggable="true" cx="10" cy="2" r="2" fill="#C4C4C4" /><circle draggable="true" cx="10" cy="10" r="2" fill="#C4C4C4" /></svg>';
 const deleteSvg = '<svg class="delete" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.5" y="0.5" width="19" height="19" rx="9.5" stroke="#C4C4C4" /><path d="M6 6L14 14" stroke="#C4C4C4" /><path d="M6 14L14 6" stroke="#C4C4C4" /></svg>';
 
-const changeSortSvg = (type) => {
-    if (type == reverseSort) {
+const opositeSort = (type) => {
+    if (type == oposideSide) {
         sortingWrapper.innerHTML = sort;
     }
     else if (type == sort) {
-        sortingWrapper.innerHTML = reverseSort;
+        sortingWrapper.innerHTML = oposideSide;
     }
 }
 
 const deleteButtons = document.querySelectorAll('.delete');
 deleteButtons.forEach((item) => {
-    item.addEventListener('click', deleteRow);
+    item.addEventListener('click', deletingElements);
 });
 
-function deleteRow(e) {
+function deletingElements(e) {
     if (!e.target.matches('.delete')) return;
     e.target.parentElement.parentElement.removeChild(e.target.parentElement);
 }
-function newRow() {
+function addingTask() {
     const block = document.querySelector('#block-to-do');
-    const newRow = document.createElement('li');
-    newRow.innerHTML = `${moverSvg}<input class="row-info" type="text">${deleteSvg}`;
-    newRow.className = 'row-to-do';
-    newRow.draggable = 'true';
-    block.appendChild(newRow);
-    newRow.childNodes[2].addEventListener('click', deleteRow);
+    const addingTask = document.createElement('li');
+    addingTask.innerHTML = `${moverSvg}<input class="row-info" type="text">${deleteSvg}`;
+    addingTask.className = 'row-to-do';
+    addingTask.draggable = 'true';
+    block.appendChild(addingTask);
+    addingTask.childNodes[2].addEventListener('click', deletingElements);
     dragDrop();
 }
 
 const SetButtonSymbol = () => {
     const svg = document.querySelector('svg');
     if (svg.id == 'top-sort') {
-        changeSortSvg(sort);
+        opositeSort(sort);
         return true;
     } else {
-        changeSortSvg(reverseSort);
+        opositeSort(oposideSide);
         return false;
     }
 }
@@ -120,4 +120,4 @@ function dragDrop() {
 }
 
 dragDrop();
-newRow();
+addingTask();
